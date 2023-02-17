a8842887fba8065aea68f5acd5587c862cf07a92
upoudel888 <upoudel888@gmail.com>
2023-02-17 21:37:08 +0545

    changed the static files stuff

--- monumentDetect/settings.py
+++ monumentDetect/settings.py
@@ -52,8 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
+    "corsheaders.middleware.CorsMiddleware"
+    # "whitenoise.middleware.WhiteNoiseMiddleware"
     
 ]
 # change this in production
@@ -146,5 +146,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "mediafolder")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
